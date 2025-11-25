d986808c2ca7f0cabcec42d9d20b732883113a38
Aman <amanbhudia639@gmail.com>
2025-11-25 15:18:35 +0000

    changes vue config folder

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/FRONTEND/'
 })
 
-module.exports = {
-  publicPath: '/FRONTEND/'
-}
